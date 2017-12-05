b827c3224f88c3f45f2b1d611529804c2f9e02a7
Roman Gräf <roman.graef@gmail.com>
2017-12-05 12:56:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,13 +10,3 @@ $(()=>{
 `);
 });
 
-
-$(window).resize(()=>{
-  $('#board').css({
-    position: absolute,
-    left: ($(window).width() - $('#board').outerWidth())/2,
-    top: ($(window).height() - $('#board').outerHeight())/2,
-  });
-});
-
-$(window).resize();
